e9fbf51436fb4116c65043aabb5af3ee70aeab42
Asadulloh <asadulloh857@gmail.com>
2025-06-24 09:49:59 +0500

    first commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white rounded-xl p-6 w-[400px] shadow-lg relative">
         <button
           className="absolute top-2 right-3 text-gray-500 hover:text-red-500 text-2xl"
